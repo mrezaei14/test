b480f3ac6ad32493cdd77ae54641106a019bcfee
Majid <mrezaei14@gmail.com>
2022-07-21 15:56:54 +0430

    new

---
+++ q4.py
@@ -0,0 +1,18 @@
+
+# # 
+# numbers = [4,6,1,9,3]
+
+new = sorted(numbers)
+print(new[-2])
+
+# new = sorted(numbers , reverse=True)
+# print(new[1])
+
+
+# new = list(reversed(numbers))
+# print(new[-2])
+
+
+
+x,y,z = 1,2,3
+print(x,y,z , sep='_')
